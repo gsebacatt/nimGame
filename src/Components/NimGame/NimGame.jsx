d632be7093827e2c8f913152a74c1d162629ad99
Seba <gsebacatt@gmail.com>
2021-09-18 18:17:54 -0400

    Initial version, non working game

---
+++ src/Components/NimGame/NimGame.jsx
@@ -0,0 +1,175 @@
+import React, {Component} from "react";
+import {minimaxDepth} from "./nimFunctions";
+import '../../style.css';
+
+
+export default class NimGame extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            gameState: [],
+            linesNumber: 0,
+            currentRow: null,
+            errorMessage: null,
+            aiTurn: false,
+        }
+    }
+
+
+    componentDidMount() {
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+    }
+
+    submitLines = (value) => {
+        this.setState({
+            linesNumber: +value.target.value,
+            //gameState: new Array(Number(value.target.value)),
+        })
+    }
+
+    getLinesLoader = () => {
+        let content = [];
+
+        for (let i = 0; i < this.state.linesNumber; i++) {
+            content.push(<li key={i}><span>Linea {i} : </span><input type="number" id={i}
+                                                                     onBlur={(value) => this.loadGameState(i, value)}/>
+            </li>)
+        }
+
+        return content;
+    }
+
+    loadGameState = (i, value) => {
+        this.setState({
+            gameState: [...this.state.gameState, +value.target.value]
+        })
+    }
+
+    generateNim = () => {
+        let content = [];
+
+        if (this.state.gameState.length > 0) {
+            for (let i = 0; i < this.state.linesNumber; i++) {
+                content.push(<tr key={i} id={'row' + i}
+                                 className='row'>{this.generateRow(this.state.gameState[i], i)}</tr>)
+            }
+        }
+
+
+        return content;
+    }
+
+    generateRow = (number, row) => {
+        let content = [];
+        for (let i = 0; i < number; i++) {
+            content.push(<td key={i} id={'elem_' + i} data={row} className='spot'
+                             onClick={(i) => this.clickElement(i, row)}></td>)
+        }
+        return content;
+    }
+
+    /*Este metodo responde a la jugada del usuario*/
+    clickElement = (event, row) => {
+        const {currentRow} = this.state
+
+        if (currentRow === row || currentRow === null) {
+            //Solo movimientos en la misma fila ya seleccionada
+
+            this.setState(state => {
+                const list = this.state.gameState.map((rowVal, index) => {
+                    return row === index ? rowVal - 1 : rowVal;
+                })
+                return {
+                    currentRow: row,
+                    gameState: list,
+                }
+            })
+        } else {
+            this.setState({
+                errorMessage: "Solo puede eliminar de la misma fila",
+            })
+        }
+    }
+
+    /*Este metodo calcula y ejecuta la jugada de la IA*/
+    playAi = () => {
+        //bloquear boton de usuario
+        this.setState({
+            currentRow: null,
+            errorMessage: "",
+            aiTurn: true,
+        }, () =>{
+            //Ejecutar llamada a algoritmo, inicialmente minimax, luego parametrizado
+            let bestMove = minimaxDepth(this.state.gameState, 3, true);
+            //let bestMove = alphaBetaPruning(this.state.gameState,-10000,+10000, 3, true);
+
+            this.setState(state => {
+                const list = this.state.gameState.map((rowVal, index) => {
+                    return bestMove.row === index ? rowVal - bestMove.amount : rowVal;
+                })
+                return {
+                    gameState: list,
+                    aiTurn: false,
+                }
+            })
+
+        //Finalmente desbloquear boton para nueva jugada de usuario
+            //Controlar estado del juego
+    })
+
+    }
+
+    resetGame = () => {
+        this.setState({
+            linesNumber: 0,
+            gameState: [],
+        })
+    }
+
+
+    render() {
+        const {aiTurn} = this.state;
+        return (
+            <div className={""}>
+                <h1>Nim</h1>
+                <div id='selection'>
+                    Numero de Lineas :
+                </div>
+                <input type="number" id="linesNumber" onChange={this.submitLines}/>
+
+                {this.state.linesNumber > 0 && (
+                    <>
+                        <ul>
+                            {this.getLinesLoader()}
+                        </ul>
+
+                        <button className={"hover"} disabled={aiTurn}
+                                onClick={this.playAi}>Jugar
+                        </button>
+                        <button className={"hover"} disabled={!aiTurn}>IA</button>
+                    </>
+                )}
+
+
+                <p>Estado del Juego: <span>{this.state.gameState.map(state => (<>{state} ,</>))}</span></p>
+
+
+                <div id='result'>
+                </div>
+
+                <div id="error" style={{color: "red"}}>
+                    {this.state.errorMessage !== null ? this.state.errorMessage : ""}
+                </div>
+                <table>
+                    {this.generateNim()}
+                </table>
+
+
+            </div>
+        )
+    }
+}
+
---
+++ src/Components/NimGame/nimFunctions.js
@@ -0,0 +1,173 @@
+//Ajustar para minimax de busqueda limitada
+//para el caso de nim que deberia retornar la funcion(que hay en cada ply)
+//podria ser un objeto {row: , amount:, score: } cuanto sacar de que fila
+//y minimax recibe el gameState y la profundidad, y el player como diferenciar?
+//Se puede difernciar con un boolean isMax = true o isMax = false
+//y en la primera llamada de la pila se manda isMax =True porque es la IA
+export function minimaxDepth(game, depth, isMax) {
+
+    //console.log(game)
+    //console.log("depth:  " + depth)
+    //console.log("isMax:     " + isMax)
+
+    //Terminal State check comes first
+    //Tener en cuenta que este juego no se puede empatar, gana uno o el otro
+    if(depth === 0  || winning(game)){
+        if(isMax){
+            return {score: 10}
+        }else{
+            return {score: -10}
+        }
+    }
+
+    //En este array se almacenaran las jugadas posibles
+    let moves = [];
+
+    for (let j = 0; j < game.length; j++) {
+        //loop in range from 0 to game.lenght[i]
+        Array(game[j]).fill(0).forEach((_, i) => {
+            let move = {}
+            move.row = j;
+            move.amount = i + 1;
+
+            console.log(game)
+            console.log(j)
+            console.log(i + 1)
+
+            game[j] = game[j] - move.amount;
+
+            console.log(game)
+
+            let result = minimaxDepth( game, depth - 1, !isMax);
+
+            move.score = result.score;
+
+            //restore value
+            game[j] = game[j] + move.amount;
+
+            moves.push(move);
+        });
+    }
+
+    console.log("moves :  ")
+    console.log(moves)
+
+    let bestMove;
+    //El prototipo de move deberia ser {row: , amount:, score: }
+    //El valor de score solo se usa dentro de minimax
+    //El resto sirve para concretar la jugada
+
+    //Casos de llamadas recursivas para max y min
+    if (isMax) {
+        let bestScore = -100000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        let bestScore = 100000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+
+    }
+
+    //console.log("bestMove: ")
+    //console.log(moves[bestMove])
+
+    return moves[bestMove];
+}
+
+/*Define quien gana si el juego es normal o misėre*/
+function winning(gameState) {
+    let sum = 0;
+    gameState.forEach(line => {
+        sum += line;
+    })
+
+   //console.log("sum: " + sum)
+    if (
+        sum === 0
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+export function alphaBetaPruning(game, depth, alpha, beta, isMax){
+    if(depth === 0  || winning(game)){
+        if(isMax){
+            return {score: 10}
+        }else{
+            return {score: -10}
+        }
+    }
+
+    let moves = [];
+
+    for (let j = 0; j < game.length; j++) {
+        Array(game[j]).fill(0).forEach((_, i) => {
+            let move = {}
+            move.row = j;
+            move.amount = i + 1;
+
+            console.log(game)
+            console.log(j)
+            console.log(i + 1)
+
+            game[j] = game[j] - move.amount;
+
+            console.log(game)
+
+            let result = alphaBetaPruning( game, depth - 1,alpha, beta, !isMax);
+
+            move.score = result.score;
+
+            //restore value
+            game[j] = game[j] + move.amount;
+
+            moves.push(move);
+        });
+    }
+
+    console.log("moves :  ")
+    console.log(moves)
+
+    let bestMove;
+
+    if (isMax) {
+        let bestScore = -100000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+                if(bestScore > beta){
+                    break;
+                }
+                alpha = Math.max(alpha, bestScore)
+            }
+        }
+    } else {
+        let bestScore = 100000;
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+                if(bestScore < alpha){
+                    break;
+                }
+                beta = Math.min(beta, bestScore)
+            }
+        }
+
+    }
+
+    return moves[bestMove];
+}

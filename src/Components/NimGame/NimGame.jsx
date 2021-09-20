fe2e6e885c23da0a42b0b776a857a8bdfa76e413
Seba <gsebacatt@gmail.com>
2021-09-20 01:07:21 -0400

    Added rl code and improved UI options

--- src/Components/NimGame/NimGame.jsx
+++ src/Components/NimGame/NimGame.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
-import {minimaxDepth} from "./nimFunctions";
+import {minimaxDepth, alphaBetaPruning} from "./nimFunctions";
 import '../../style.css';
+import {rl} from "./nimAiFunctions";
 
 
 export default class NimGame extends Component {
@@ -10,11 +11,14 @@ export default class NimGame extends Component {
         this.state = {
             gameState: [],
             linesNumber: 0,
+            depth : 0,
             currentRow: null,
             errorMessage: null,
             aiTurn: false,
             gameDone: false,
             result: null,
+            algo: null,
+            training: false,
         }
     }
 
@@ -28,6 +32,13 @@ export default class NimGame extends Component {
     submitLines = (value) => {
         this.setState({
             linesNumber: +value.target.value,
+            //gameState: new Array(Number(value.target.value)),
+        })
+    }
+
+    submitDepth = (value) => {
+        this.setState({
+            depth: +value.target.value,
             //gameState: new Array(Number(value.target.value)),
         })
     }
@@ -113,8 +124,19 @@ export default class NimGame extends Component {
             aiTurn: true,
         }, () => {
             //Ejecutar llamada a algoritmo, inicialmente minimax, luego parametrizado
-            let bestMove = minimaxDepth(this.state.gameState, 3, true);
-            //let bestMove = alphaBetaPruning(this.state.gameState,-10000,+10000, 3, true);
+
+
+
+            let bestMove;
+            switch (this.state.algo){
+                case "minimax":
+                    bestMove = minimaxDepth(this.state.gameState, this.state.depth, true);
+                    break;
+                case "alphabeta":
+                    bestMove = alphaBetaPruning(this.state.gameState,-10000,+10000, this.state.depth, true);
+                case "rl":
+                    bestMove = rl(this.state.gameState);
+            }
 
             this.setState(state => {
                 const list = this.state.gameState.map((rowVal, index) => {
@@ -141,6 +163,7 @@ export default class NimGame extends Component {
             linesNumber: 0,
             gameState: [],
             result: null,
+            algo: null
         })
     }
 
@@ -157,12 +180,25 @@ export default class NimGame extends Component {
         }
     }
 
+    selectAlgo = (event) => {
+        this.setState({
+            algo: event.target.value,
+        })
+    }
+
 
     render() {
         const {aiTurn} = this.state;
         return (
             <div className={""}>
                 <h1>Nim</h1>
+
+                <div id="radio-selection" onChange={this.selectAlgo}>
+                        <input type="radio" value="minimax" name={"algo"} checked={this.state.algo === "minimax"}/> Minimax
+                        <input type="radio" value="alphabeta" name={"algo"} checked={this.state.algo === "alphabeta"}/> Poda Alfa Beta
+                        <input type="radio" value="rl" name={"algo"} checked={this.state.algo === "rl"}/> Reinforcement Learning
+                </div>
+                <hr />
                 <div id='selection'>
                     Numero de Lineas :
                 </div>
@@ -183,6 +219,12 @@ export default class NimGame extends Component {
                 )}
 
 
+                {(this.state.algo === "minimax" || this.state.algo === "alphabeta") && (
+                    <p>
+                        Profundidad:
+                        <input type="number" id="depth" onChange={this.submitDepth} />
+                    </p>
+                )}
                 <p>Estado del Juego: <span>{this.state.gameState.map(state => (<>{state} ,</>))}</span></p>
 
 
---
+++ src/Components/NimGame/nimAiFunctions.js
@@ -0,0 +1,171 @@
+import {winning} from "./nimFunctions";
+import NimGame from "./NimGame";
+
+export function train(n) {
+
+    let player = nimAi()
+
+
+    for (let i = 0; i < n; i++) {
+        let game = nimGame()
+
+        let last = {
+            0: {"state": null, "action": null},
+            1: {"state": null, "action": null}
+        }
+
+        while (true) {
+
+            let state = [...game.piles] //copia del game state
+            let action = player.choose_action(game.piles, game)
+
+            last[game.player]["state"] = state
+            last[game.player]["action"] = action
+
+            game.move(action);
+            let new_state = [...game.piles];
+
+            if (game.winner !== null) {
+                player.update(state, action, new_state, -1, game)
+                player.update(last[game.player]["state"],
+                    last[game.player]["action"],
+                    new_state,
+                    1, game)
+                break;
+            } else if (last[game.player]["state"] !== null) {
+                player.update(
+                    last[game.player]["state"],
+                    last[game.player]["action"],
+                    new_state,
+                    0, game
+                )
+            }
+
+        }
+    }
+    console.log("training terminado")
+
+    //Retorna la IA entrenada
+    return player;
+}
+
+function nimAi(alpha = 0.5, epsilon = 0.1) {
+
+    this.q = {}; //la tabla q, seria un diccionario
+    this.alpha = alpha;
+    this.epsilon = epsilon;
+
+    this.update = (old_state, action, new_state, reward, game) => {
+        let old = this.get_q_value(old_state, action);
+        let best_future = this.best_future_reward(new_state, game)
+        this.update_q_value(old_state, action, old, reward, best_future)
+    }
+
+    this.get_q_value = (state, action) => {
+        if (this.q.includes({state, action})) {
+            return {state, action} //No estoy seguro de esta sintaxis
+        } else {
+            return 0;
+        }
+    }
+
+    //Aplicacion de formula de q_learning
+    this.update_q_value = (state, action, old_q, reward, future_rewards) => {
+        let index = this.q.findIndex({state, action});
+        this.q[index] = old_q + this.alpha * (reward + future_rewards - old_q)
+    }
+
+
+    this.best_future_reward = (state, game) => {
+        let best_reward = 0;
+
+        game.available_actions([...state]).forEach(action => {
+            best_reward = Math.max(this.get_q_value(state, action));
+        })
+
+        return best_reward;
+
+    }
+    this.choose_action = (state, epsilon = true, game) => {
+        let best_action = null;
+        let best_reward = 0;
+        let actions = [...game.available_actions([...state])];
+
+        actions.forEach( action => {
+            if(best_action === null || this.get_q_value(state,action) > best_reward){
+                best_reward = this.get_q_value(state,action);
+                best_action = action;
+            }
+        })
+
+        if(epsilon) {
+            actions.forEach(action => {
+                let weights;
+                if (action === best_action) {
+                    weights = [1 - epsilon];
+                } else {
+                    weights = epsilon / (actions.length - 1)
+                }
+            })
+
+            best_action =  actions[Math.floor(Math.random() * actions.length)];
+        }
+
+        return best_action;
+    }
+}
+
+//Para el entrenamiento nimGame deberia recibir el estado del juego
+//solicitado por el usuario en nimGame.jsx, (1,3,5,7) es solo un placeholder
+function nimGame(initial = [1, 3, 5, 7]) {
+    this.player = 0;
+    this.piles = [];
+    this.winner = null;
+
+    this.available_actions = (piles) => {
+
+        //Set nativo de ES6, no probar con ES5!
+        let actions = new Set();
+
+        for (let i = 0; i < piles.length; i++) {
+            Array(piles).fill(0).forEach((_, j) => {
+                actions.add({i, j});
+            })
+        }
+    }
+
+    this.other_player = (player) => {
+        return player === 1 ? 0 : 1;
+    }
+
+    //Cambia al otro turno (ya que el jugador sigue siendo el mismo)
+    this.switch_player = () => {
+        this.player = this.other_player(this.player)
+    }
+
+    this.move = (action) => {
+        const {pile, count} = action;
+
+        if (this.winner !== null) {
+            console.log('Juego terminado');
+        }
+        if (pile < 0 || pile >= this.piles.length) {
+            console.log("pila invalida");
+        }
+        if (count < 1 || count > this.piles[pile]) {
+            console.log("Numero de objetos no valido")
+        }
+
+
+        this.piles[pile] -= count;
+        this.switch_player();
+
+        if (winning(this.piles)) {
+            this.winner = this.player;
+        }
+    }
+}
+
+export function rl(gameState){
+    return {};
+}
